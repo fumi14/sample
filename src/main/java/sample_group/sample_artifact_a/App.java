44799af7a6095dc1a573732c2a26ca81c5835db7
fumi14 <nkgw14@gmail.com>
2017-09-22 02:17:51 +0900

    a

---
+++ src/main/java/sample_group/sample_artifact_a/App.java
@@ -0,0 +1,11 @@
+package sample_group.sample_artifact_a;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
